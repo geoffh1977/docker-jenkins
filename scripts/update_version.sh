988ea4130a32ffbf33de011fb49bb1b782cc641a
Geoffrey Harrison <geoffh1977@gmail.com>
2019-05-09 15:08:08 +1000

    Update Repo Format

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -3,14 +3,14 @@
 
 # Get The Versions Of The Software
 SITE_VERSION=$(curl -s http://mirrors.jenkins.io/war-stable/ | grep -Eo "[0-9]{1,2}\.[0-9]{1,4}\.[0-9]{1,4}" | tail -n1)
-LOCAL_VERSION=$(grep "finalImageVersion" container.conf | cut -d= -f 2)
+LOCAL_VERSION=$(grep "finalImageVersion" config.yml | cut -d: -f 2 | sed 's/^ //g')
 
 # Check Versions And Update File
 if [ "$SITE_VERSION" != "$LOCAL_VERSION" ]
 then
-  sed -i "s/^finalImageVersion=.*/finalImageVersion=${SITE_VERSION}/" container.conf
+  sed -i "s/^finalImageVersion:.*/finalImageVersion:\ ${SITE_VERSION}/" config.yml
   SHA=$(curl -s "http://mirrors.jenkins.io/war-stable/${SITE_VERSION}/jenkins.war.sha256" | awk '{print $1}')
-  sed -i "s/^jenkinsSha256=.*/jenkinsSha256=${SHA}/" container.conf
+  sed -i "s/^jenkinsSha256:.*/jenkinsSha256:\ ${SHA}/" config.yml
   echo " Version Updated."
 else
   echo " No Version Change."
